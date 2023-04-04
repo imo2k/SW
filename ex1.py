0bfa79078b0cb6144c0369ae50d5758ea0fb2aae
o2k <127279971+imo2k@users.noreply.github.com>
2023-04-04 13:15:49 +0900

    Add files via upload

---
+++ ex1.py
@@ -0,0 +1,7 @@
+first=input("input num1: ") #첫번째 정수 입력
+second=input("input num2: ") #두번째 정수 입력
+if first>second: # 선택 조건
+    print("Big number is : ", first) # 조건이 참일 때 실행
+else:
+    print("Big number is : ", second) # 조건이 거짓일 때 실행
+    
